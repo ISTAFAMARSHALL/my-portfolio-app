2c98beeb7b9cb71edea0cc1fa791fa7171609789
istafamarshall <istafa.a.marshall@gmail.com>
2024-06-26 23:32:54 -0400

    update project detail page

--- app/projects/[slug]/page.js
+++ app/projects/[slug]/page.js
@@ -1,74 +1,108 @@
 import Image from "next/image";
 import Link from 'next/link';
-import ImageSlideShow from '@/components/image-slide-show'
-import smartBuyImg from '@/public/images/SmartBuy Screenshot .jpeg'
-import stimsimg from '@/public/images/S.T.I.M.S Screenshot .jpeg'
-import maybellineImg from '@/public/images/Maybelline Catelog Screenshot.jpeg'
-import reservationappImg from '@/public/images/Reservation App Screenshot.jpeg'
+import ImageSlideShow from '@/components/image-slide-show';
+import smartBuyImg from '@/public/images/SmartBuy Screenshot .jpeg';
+import stimsimg from '@/public/images/S.T.I.M.S Screenshot .jpeg';
+import maybellineImg from '@/public/images/Maybelline Catelog Screenshot.jpeg';
+import reservationappImg from '@/public/images/Reservation App Screenshot.jpeg';
 
-export default function ProjectDetailsPage (props) {
-
+export default function ProjectDetailsPage(props) {
     const items = [
         {
             title: "Smart Buy",
             slug: "smart-buy",
-            description: "Best Buy Clone",
-            role_contributions: "Created App",
-            technology: "React, Ruby on Rails",
-            links: "",
+            description: "Smart Buy is an innovative e-commerce platform inspired by Best Buy, designed to deliver an exceptional online shopping experience. The app boasts an intuitive interface that allows users to easily navigate a comprehensive product catalog. With secure user accounts and efficient order tracking, customers can effortlessly manage their purchases. Developed with React on the front-end and Rails on the back-end, Smart Buy combines speed, reliability, and scalability to meet the demands of modern e-commerce.",
+            role_contributions: "Created by Istafa A Marshall",
+            technology: [
+                "Frontend Development: Crafted with React to provide a dynamic, responsive, and interactive user interface.",
+                "Styling: Utilized CSS to create a clean, modern, and user-friendly design, enhancing the overall user experience.",
+                "Backend Development: Powered by Rails to efficiently handle complex business logic and ensure secure transactions.",
+                "Database Management: Integrated PostgreSQL for robust and scalable data storage, supporting extensive product catalogs and user accounts."
+            ],
+            link: "",
+            demo: "",
+            github: '',
             screenshots: smartBuyImg
         },
         {
             title: "S.T.I.M.S",
             slug: "stims",
             description: "Educational app",
-            role_contributions: "Created App",
+            role_contributions: "Created by Istafa A Marshall",
             technology: "React, Ruby on Rails",
-            links: "",
+            link: "",
+            demo: "",
+            github: '',
             screenshots: stimsimg
         },
         {
             title: "Maybelline Catelog Collection",
             slug: "maybelline-catelog-collection",
             description: "Mark up Tracker",
-            role_contributions: "Created App",
+            role_contributions: "Created by Istafa A Marshall",
             technology: "React, Ruby on Rails",
-            links: "",
+            link: "",
+            demo: "",
+            github: '',
             screenshots: maybellineImg
         },
         {
             title: "Reservation App",
             slug: "reservation-app",
             description: "Restaurant Tracker",
-            role_contributions: "Created App",
+            role_contributions: "Created by Istafa A Marshall",
             technology: "React, Ruby on Rails",
-            links: "",
+            link: "",
+            demo: "",
+            github: '',
             screenshots: reservationappImg
         }
-    ]
+    ];
 
-    let item = items.filter((item) => item.slug === props.params.slug)
+    let item = items.filter((item) => item.slug === props.params.slug);
 
     return (
         <>
-            {item.map((item) => {
-                return (
-                    <div key={item.slug} style={{textAlign: 'center'}}>
-                        {/* <Image
-                            src={item.screenshots}
-                            alt={item.title}
-                        /> */}
-                        <ImageSlideShow slug={item.slug} />
-                        <h1 style={{ fontSize: '50px'}}>{item.title}</h1>
-                        <span>{item.description}</span>
-                        <br></br>
-                        <span>{item.role_contributions}</span>
-                        <br></br>
-                        <span>{item.technology}</span>
-                        <br></br>
+            {item.map((item) => (
+                <div key={item.slug}>
+                    {/* <Image src={item.screenshots} alt={item.title} /> */}
+
+                    <h1 style={{ fontSize: '50px', textAlign: 'center' }}>{item.title}</h1>
+                    
+                    <ImageSlideShow slug={item.slug} fill />
+
+                    <p style={{ margin: '10px', marginTop: '10px', fontFamily: 'cursive', fontSize: '20px', textAlign: 'center' }}>{item.role_contributions}</p>
+                    
+                    <h1 style={{ textAlign: 'left', fontSize: '20px', marginTop: '40px', textDecorationLine: 'underline' }}>Description</h1>
+
+                    <p style={{margin: '15px' , textAlign: 'justify' , marginTop: '5px' }}>{item.description}</p>
+                    <br></br>
+                    
+                    <br></br>
+
+                    <h1 style={{ textAlign: 'left', fontSize: '20px', textDecorationLine: 'underline' , marginTop: '-20px' }}>Technologies</h1>
+
+                    <ul >
+                        {item.technology.map((tech, index) => (
+                            <div style={{ marginTop: '-15px'}}>
+                            <li key={index} style={{ margin: '25px' , textAlign: 'left' , marginLeft:'30px' , listStyle:'outside' }} >{tech}</li>
+                            </div>
+                        ))}
+                    </ul>
+
+                    <h1 style={{ textAlign: 'left', fontSize: '20px', textDecorationLine: 'underline' , marginTop: '30px'}}>Links</h1>
+                    
+                    <div style={{ margin: '15px' , listStyle:'outside' , marginTop: '5px'}} >
+                        <p>Live Demo:{}</p>
+                        <p>Tutorial:{}</p>
+                        <p>github:{}</p>
                     </div>
-                )
-            })}
+
+
+                    
+                    <br></br>
+                </div>
+            ))}
         </>
-    )
+    );
 }
